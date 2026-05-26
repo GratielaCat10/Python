782cd66cca89942dbc2d06430e082aba0cf0f068
GratielaCat10 <gratielapascal94@gmail.com>
2026-05-26 13:16:43 +0300

    Update 3_Sets.py

--- 03. Data Structures/3_Sets.py
+++ 03. Data Structures/3_Sets.py
@@ -1,1 +1,33 @@
+""" Exercise 1: Check if list has duplicates. """
+
+numbers = [1, 2, 3, 4, 4]
+
+if len(numbers) == len(set(numbers)):
+    print("All unique")
+else:
+    print("Duplicates found")
+
+
+
+""" Exercise 2: Check if a username exists in a system (fast lookup). """
+
+users = {"ana", "ion", "maria", "alex"}
+
+name = input("Enter username: ")
+
+if name in users:
+    print("User exists")
+else:
+    print("User not found")
+
+
+
+""" Exercise 3: Count unique website visitors. """
+
+visits = ["u1", "u2", "u1", "u3", "u2", "u4"]
+
+unique_visitors = set(visits)
+
+print("Unique visitors:", len(unique_visitors))
+
 
